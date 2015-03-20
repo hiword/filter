1808f467f730d0b419ef27c879b1e1ea0b881aa6
simon <28737164@qq.com>
2015-03-20 10:29:59 +0800

    add FilterInterface.php XssEditor.php

---
+++ src/Component/XssEditor.php
@@ -0,0 +1,11 @@
+<?php
+namespace Filter\Component;
+
+use Filter\FilterInterface;
+class XssEditor implements FilterInterface {
+	
+	public function filter($data) {
+		
+	}
+	
+}
---
+++ src/FilterInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace Filter;
+interface FilterInterface {
+	
+	public function filter();
+	
+}
