3298c35b946114546ea1ed49064b38eafa53732f
simon <28737164@qq.com>
2015-03-20 10:28:59 +0800

    filter basic

--- src/Component/Xss.php
+++ src/Component/Xss.php
@@ -1,7 +1,37 @@
 <?php
 namespace Filter\Component;
-class Xss {
+
+use Filter\FilterInterface;
+
+class Xss implements FilterInterface {
 	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Filter\FilterInterface::filter()
+	 */
+	public function filter($data) {
+		return is_array($data) ? $this->filterArray($data) : $this->filterSingle($data);
+	}
 	
+	/**
+	 * 过滤数组
+	 * @param array $data
+	 * @return array
+	 */
+	public function filterArray(array $data) {
+		foreach ($data as &$item) {
+			$item = is_array($item) ? $this->filterArray($item) : $this->filterSingle($value);
+		}
+		return $data;
+	}
+	
+	/**
+	 * 单条数据过滤
+	 * @param unknown $value
+	 * @return string
+	 */
+	public function filterSingle($data) {
+		return strip_tags(trim($data));
+	}
 	
 }
--- src/Filter.php
+++ src/Filter.php
@@ -1,34 +1,17 @@
 <?php
 namespace Filter;
-class Filter {
+class Filters {
 	
-	protected $filterType = [
-		'xss',
-	];
+	protected $filter;
+	protected $data;
 	
-	public function get($data,array $type = array()) {
-		
+	public function __construct(FilterInterface $filter,$data) {
+		$this->filter = $filter;
+		$this->data = $data;
 	}
-	
-	/**
-	 *	 过滤数组
-	 * @param array|mixed $data
-	 * @return boolean
-	 */
-	public function filters(array $data) {
-		foreach ($data as &$value) {
-			$value = is_array($value) ? $this->filters($value) : $this->filter($value);
-		}
-		return $data;
-	}
-	
-	/**
-	 * 单条数据过滤
-	 * @param unknown $value
-	 * @return string
-	 */
-	public function filter($value) {
-		return strip_tags($value);
+
+	public function filter() {
+		return $this->filter->filter($this->data);
 	}
 	
 }
